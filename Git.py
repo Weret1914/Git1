54172dfd6b04a2bbfa3d71a1d932f915c18945b5
Марат Акимов <maratakimov2005@gmail.com>
2023-12-02 15:12:08 +0300

    1

--- Git.py
+++ Git.py
@@ -2,7 +2,7 @@
 word = "Some"  # String
 number = 7  # Integer
 num_2 = 5.68132  # Float
-bool = False  # Boolean
+status_1 = False  # Boolean
 
 word = "Результат:"
 
@@ -13,3 +13,4 @@ res = number % x
 num_3, w, status = 12, "word", True
 
 print(res,num_3,word, status)
+
---
+++ Untitled-1.js
@@ -0,0 +1,3 @@
+let main_str ="Я изучаю JavaScript"
+console.log(main_str[0]) 
+alert(main_str)
